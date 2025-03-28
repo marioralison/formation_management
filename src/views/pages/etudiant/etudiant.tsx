f33435b56d57586ea46ba947e088127d98ee5a3a
Mario Ralison <marioeuphraim@gmail.com>
2025-03-28 23:22:19 +0300

    Add UI in student page

---
+++ src/views/pages/etudiant/etudiant.tsx
@@ -0,0 +1,203 @@
+import Remove from '../../../assets/Remove.png'
+
+function Etudiant() {
+
+  return (
+    <div className="h-full w-full pl-12 pr-12 pt-6 pb-6 bg-white rounded-2xl grid grid-row-10 gap-1">
+      <div className="flex justify-end items-center gap-4 row-span-2">
+        <div className="w-10 h-10 flex rounded-full justify-center items-center bg-slate-900">
+          <h1 className="text-white text-xl font-bold">5</h1>
+        </div>
+        <h1 className="font-bold text-2xl">Etudiant</h1>
+      </div>
+      <div className="row-span-8 overflow-scroll">
+        <table className="w-full h-auto table-fixed border-collapse">
+          <thead className="border-b-2 border-black-200">
+            <tr className="h-14 text-center">
+              <th>Numéro</th>
+              <th>Nom</th>
+              <th>Prénom</th>
+              <th>Date naissance</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+            <tr className="h-14 text-center">
+              <td>01</td>
+              <td>Ralison</td>
+              <td>Mario</td>
+              <td>15/12/2004</td>
+              <td className="text-blue-400">marioralison@gmail.com</td>
+              <td className='text-red-400 grid place-items-center pt-3'>
+                <img className='w-8 h-8' src={Remove} alt="icon-remove" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Etudiant;
