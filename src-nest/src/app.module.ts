f993fe9c340e47c5825042d6153d0836f636c826
TsitouhRanjafy <radohery09@gmail.com>
2025-04-11 18:44:33 +0200

    init student

--- src-nest/src/app.module.ts
+++ src-nest/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { StudentsModule } from './students/students.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development',
-    })
+    }),
+    StudentsModule
   ],
   controllers: [AppController],
   providers: [AppService],
